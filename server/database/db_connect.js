4b2972dbd0d8f0b4407bd2f3b9269a61dc2da74d
Abel R <abelprivasq@gmail.com>
2024-07-12 18:12:37 -0400

    limit and orderBY working V1.0.0

---
+++ server/database/db_connect.js
@@ -0,0 +1,23 @@
+import pg from 'pg'
+import 'dotenv/config'
+
+const { Pool } = pg
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  allowExitOnIdle: true
+}
+const pool = new Pool(config)
+const db = async (query, values) => {
+  try {
+    const result = await pool.query(query, values)
+    console.log('Respuesta exitosa')
+    return result.rows
+  } catch (error) {
+    console.error('db_connetc => db', error)
+  }
+}
+export default db
